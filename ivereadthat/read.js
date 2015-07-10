ab63c3957af1a2324e903a8f924dafcbc538b041
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2015-07-10 11:05:56 +0800

    寫成config

--- ivereadthat/read.js
+++ ivereadthat/read.js
@@ -1,21 +1,27 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver
+var config = {
+  like     : "朕知道了",
+  unlike   : "朕也是看報紙才知道",
+  comment  : "卿家可知？",
+  share    : "宣旨",
+};
 var observer = new MutationObserver(function(mutations, observer) {
     jQuery(".UFILikeLink").each(function() {
         if(this.dataset.ft == '{"tn":">"}') {
-            $(this).html("朕知道了")
+            $(this).html(config.like);
         }
         else if(this.dataset.ft == '{"tn":"?"}') {
-            $(this).html("朕也是看報紙才知道")
+            $(this).html(config.unlike);
         }
     })
     jQuery(".uiLinkButtonInput").each(function() {
-        if (this.value == "Comment" || this.value == "留言") {
-            this.value = "卿家可知？"
+        if (this.value == "Comment" || this.value == "留言" || this.value == '回應') {
+            this.value = config.comment;
         }
     })
     jQuery(".share_action_link").each(function() {
         if ($(this).text() == "Share" || $(this).text() == "分享") {
-            $(this).text("宣旨")
+            $(this).text(config.share);
         }
     })
 })
